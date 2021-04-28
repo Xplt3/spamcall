9251f6311d2907a1593051104a4322d5eabf176c
Xplt3 <81273481+Xplt3@users.noreply.github.com>
2021-04-28 12:22:21 +0800

    Update spam.py

--- spam.py
+++ spam.py
@@ -1,50 +1,7 @@
-#mau recode ijin dulu dong cape saya buatnya
-#ijin nya ke telegram t.me/Xplt3
-#klo gk ijin moga aja script nya rusak :v
-#=========================================
-import requests,json,os,time
-
-os.system("clear")
-banner = """
-\t======================================
-\t=         =  SPAM TELEPON =          =
-\t======================================
-\t=  [•]Creator : xplt3                =
-\t=  [•]Telegram : t.me/Xplt3          =
-\t=  [•]Result : 0=gagal / 1=berhasil  =
-\t=  [•]Example : 87*********          =
-\t=  [•]Pesan : Gunakan dengan bijak   =
-\t======================================
-\t=    = Sederhana namun istimewa =    =
-\t======================================
-"""
-print(banner)
-nomor = input("\t[•] Nomor: ")
-
-ua={
-"Host":"id.jagreward.com",
-"Connection":"keep-alive",
-"Accept":"*/*",
-"X-Requested-With":"XMLHttpRequest",
-"sec-ch-ua-mobile":"?1",
-"Save-Data":"on",
-"User-Agent":"Mozilla/5.0 (Linux; Android 6; Oneplus One) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Mobile Safari/537.36",
-"Sec-Fetch-Site":"same-origin",
-"Sec-Fetch-Mode":"cors",
-"Sec-Fetch-Dest":"empty",
-"Referer":"https://id.jagreward.com/member/register/",
-"Accept-Encoding":"gzip, deflate, br",
-"Accept-Language":"id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
-'Cookie':'PHPSESSID=jdsjb1lojb8fmn0ravo2uo8ktu; DAPROPS="sjs.webGlRenderer:PowerVR Rogue GE8320|bjs.accessDom:1|bcookieSupport:1|bcss.animations:1|bcss.columns:1|bcss.transforms:1|bcss.transitions:1|sdevicePixelRatio:2|idisplayColorDepth:24|bflashCapable:0|bhtml.audio:1|bhtml.canvas:1|bhtml.inlinesvg:1|bhtml.svg:1|bhtml.video:1|bjs.applicationCache:0|bjs.deviceMotion:1|bjs.deviceOrientation:0|bjs.geoLocation:1|bjs.indexedDB:1|bjs.json:1|bjs.localStorage:1|bjs.modifyCss:1|bjs.modifyDom:1|bjs.querySelector:1|bjs.sessionStorage:1|bjs.supportBasicJavaScript:1|bjs.supportConsoleLog:1|bjs.supportEventListener:1|bjs.supportEvents:1|bjs.touchEvents:1|bjs.webGl:1|bjs.webSockets:1|bjs.webSqlDatabase:1|bjs.webWorkers:1|bjs.xhr:1|iorientation:0|buserMedia:1|bjs.battery:0"; _ga=GA1.3.2008028289.1616460336; _gid=GA1.3.267159521.1616460336'
-}
-url = f"https://id.jagreward.com/member/verify-mobile/{nomor}/"
-req = requests.get(url,headers=ua).text
-xplt = json.loads(req)
-rs = xplt["result"] 
-ms = xplt["message"] 
-print()
-print(f"Result: {rs}")
-print()
-print(f"Message: {ms}")
-
-#Udah sampe bawah ni bro udh ijin blom kalo mau recode?
+# Jangan lupa ijin kalo mau recode :v
+# Mau Recode bro?
+# Jan di utak atik nanti rusak wkwk
+import base64
+exec(base64.b64decode("I21hdSByZWNvZGUgaWppbiBkdWx1IGRvbmcgY2FwZSBzYXlhIGJ1YXRueWEKI2lqaW4gbnlhIGtlIHRlbGVncmFtIHQubWUvWHBsdDMKI2tsbyBnayBpamluIG1vZ2EgYWphIHNjcmlwdCBueWEgcnVzYWsgOnYKIz09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09CmltcG9ydCByZXF1ZXN0cyxqc29uLG9zLHRpbWUKCm9zLnN5c3RlbSgiY2xlYXIiKQpiYW5uZXIgPSAiIiIKXHQ9PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PQpcdD0gICAgICAgICA9ICBTUEFNIFRFTEVQT04gPSAgICAgICAgICA9Clx0PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT0KXHQ9ICBb4oCiXUNyZWF0b3IgOiB4cGx0MyAgICAgICAgICAgICAgICA9Clx0PSAgW+KAol1UZWxlZ3JhbSA6IHQubWUvWHBsdDMgICAgICAgICAgPQpcdD0gIFvigKJdUmVzdWx0IDogMD1nYWdhbCAvIDE9YmVyaGFzaWwgID0KXHQ9ICBb4oCiXUV4YW1wbGUgOiA4NyoqKioqKioqKiAgICAgICAgICA9Clx0PSAgW+KAol1QZXNhbiA6IEd1bmFrYW4gZGVuZ2FuIGJpamFrICAgPQpcdD09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09Clx0PSAgICA9IFNlZGVyaGFuYSBuYW11biBpc3RpbWV3YSA9ICAgID0KXHQ9PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PQoiIiIKcHJpbnQoYmFubmVyKQpub21vciA9IGlucHV0KCJcdFvigKJdIE5vbW9yOiAiKQoKdWE9ewoiSG9zdCI6ImlkLmphZ3Jld2FyZC5jb20iLAoiQ29ubmVjdGlvbiI6ImtlZXAtYWxpdmUiLAoiQWNjZXB0IjoiKi8qIiwKIlgtUmVxdWVzdGVkLVdpdGgiOiJYTUxIdHRwUmVxdWVzdCIsCiJzZWMtY2gtdWEtbW9iaWxlIjoiPzEiLAoiU2F2ZS1EYXRhIjoib24iLAoiVXNlci1BZ2VudCI6Ik1vemlsbGEvNS4wIChMaW51eDsgQW5kcm9pZCA2OyBPbmVwbHVzIE9uZSkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzg5LjAuNDM4OS45MCBNb2JpbGUgU2FmYXJpLzUzNy4zNiIsCiJTZWMtRmV0Y2gtU2l0ZSI6InNhbWUtb3JpZ2luIiwKIlNlYy1GZXRjaC1Nb2RlIjoiY29ycyIsCiJTZWMtRmV0Y2gtRGVzdCI6ImVtcHR5IiwKIlJlZmVyZXIiOiJodHRwczovL2lkLmphZ3Jld2FyZC5jb20vbWVtYmVyL3JlZ2lzdGVyLyIsCiJBY2NlcHQtRW5jb2RpbmciOiJnemlwLCBkZWZsYXRlLCBiciIsCiJBY2NlcHQtTGFuZ3VhZ2UiOiJpZC1JRCxpZDtxPTAuOSxlbi1VUztxPTAuOCxlbjtxPTAuNyIsCidDb29raWUnOidQSFBTRVNTSUQ9amRzamIxbG9qYjhmbW4wcmF2bzJ1bzhrdHU7IERBUFJPUFM9InNqcy53ZWJHbFJlbmRlcmVyOlBvd2VyVlIgUm9ndWUgR0U4MzIwfGJqcy5hY2Nlc3NEb206MXxiY29va2llU3VwcG9ydDoxfGJjc3MuYW5pbWF0aW9uczoxfGJjc3MuY29sdW1uczoxfGJjc3MudHJhbnNmb3JtczoxfGJjc3MudHJhbnNpdGlvbnM6MXxzZGV2aWNlUGl4ZWxSYXRpbzoyfGlkaXNwbGF5Q29sb3JEZXB0aDoyNHxiZmxhc2hDYXBhYmxlOjB8Ymh0bWwuYXVkaW86MXxiaHRtbC5jYW52YXM6MXxiaHRtbC5pbmxpbmVzdmc6MXxiaHRtbC5zdmc6MXxiaHRtbC52aWRlbzoxfGJqcy5hcHBsaWNhdGlvbkNhY2hlOjB8YmpzLmRldmljZU1vdGlvbjoxfGJqcy5kZXZpY2VPcmllbnRhdGlvbjowfGJqcy5nZW9Mb2NhdGlvbjoxfGJqcy5pbmRleGVkREI6MXxianMuanNvbjoxfGJqcy5sb2NhbFN0b3JhZ2U6MXxianMubW9kaWZ5Q3NzOjF8YmpzLm1vZGlmeURvbToxfGJqcy5xdWVyeVNlbGVjdG9yOjF8YmpzLnNlc3Npb25TdG9yYWdlOjF8YmpzLnN1cHBvcnRCYXNpY0phdmFTY3JpcHQ6MXxianMuc3VwcG9ydENvbnNvbGVMb2c6MXxianMuc3VwcG9ydEV2ZW50TGlzdGVuZXI6MXxianMuc3VwcG9ydEV2ZW50czoxfGJqcy50b3VjaEV2ZW50czoxfGJqcy53ZWJHbDoxfGJqcy53ZWJTb2NrZXRzOjF8YmpzLndlYlNxbERhdGFiYXNlOjF8YmpzLndlYldvcmtlcnM6MXxianMueGhyOjF8aW9yaWVudGF0aW9uOjB8YnVzZXJNZWRpYToxfGJqcy5iYXR0ZXJ5OjAiOyBfZ2E9R0ExLjMuMjAwODAyODI4OS4xNjE2NDYwMzM2OyBfZ2lkPUdBMS4zLjI2NzE1OTUyMS4xNjE2NDYwMzM2Jwp9CnVybCA9IGYiaHR0cHM6Ly9pZC5qYWdyZXdhcmQuY29tL21lbWJlci92ZXJpZnktbW9iaWxlL3tub21vcn0vIgpyZXEgPSByZXF1ZXN0cy5nZXQodXJsLGhlYWRlcnM9dWEpLnRleHQKeHBsdCA9IGpzb24ubG9hZHMocmVxKQpycyA9IHhwbHRbInJlc3VsdCJdIAptcyA9IHhwbHRbIm1lc3NhZ2UiXSAKcHJpbnQoKQpwcmludChmIlJlc3VsdDoge3JzfSIpCnByaW50KCkKcHJpbnQoZiJNZXNzYWdlOiB7bXN9IikKCiNVZGFoIHNhbXBlIGJhd2FoIG5pIGJybyB1ZGggaWppbiBibG9tIGthbG8gbWF1IHJlY29kZT8K"))
+# Ijinnya ke t.me/Xplt3
+# Udh ampe bawah ni dah ijin blom
